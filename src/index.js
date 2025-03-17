d70b65d8b1be8096e5b84cd22714b651aa9732a5
AuraCat <136636937+AuraCat66@users.noreply.github.com>
2025-03-17 12:11:31 +0100

    add shop and restructure code

--- src/index.js
+++ src/index.js
@@ -1,22 +1,66 @@
 "use strict";
 let cookies = 0;
-
 let autocloners = 0;
-let autoclonerCost = 100;
-let autoclonerInterval = 1000;
-let autoclonerUnlockThreshold = 10;
 
-document.addEventListener("DOMContentLoaded", function (_ev) {
+let defaultPrices = {
+  autocloner: 100,
+};
+let currentPrices = { ...defaultPrices };
+
+let unlocked = {
+  autocloners: false,
+};
+
+let unlockThresholds = {
+  autocloner: 10,
+};
+let intervals = {
+  autocloner: 1000,
+};
+
+document.addEventListener("DOMContentLoaded", (_ev) => {
   gameLogic();
-});
+}, { once: true });
 
 function gameLogic() {
-  const cookieCounter = document.getElementById("cookieCounter");
+  updateShopPrices();
+
   const cloneButton = document.getElementById("cloneButton");
 
-  cloneButton.addEventListener("click", function (_ev) {
+  cloneButton.addEventListener("click", (_ev) => {
     console.log("Button clicked!");
     cookies += 1;
-    cookieCounter.innerHTML = cookies;
+    updateCookies();
+
+    checkUnlockables();
+    updateShopVisibility();
   });
+}
+
+function updateCookies() {
+  const cookieCounter = document.getElementById("cookieCounter");
+
+  cookieCounter.innerHTML = cookies;
+}
+
+function checkUnlockables() {
+  if (cookies >= unlockThresholds.autocloner && !unlocked.autocloners) {
+    unlocked.autocloners = true;
+  }
+}
+
+function updateShopVisibility() {
+  if (unlocked.autocloners) {
+    document.getElementById("shopAutoCloner").hidden = false;
+  }
+
+  Object.values(unlocked).forEach((unlocked) => {
+    if (unlocked) {
+      document.getElementById("shop").hidden = false;
+    }
+  });
+}
+function updateShopPrices() {
+  document.getElementById("shopAutoClonerCost").innerText =
+    currentPrices.autocloner;
 }
