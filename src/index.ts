4633582572a8030b016e73200994178ccb32aebd
AuraCat <136636937+AuraCat66@users.noreply.github.com>
2025-03-17 23:24:38 +0100

    implement autosaving every 3 seconds

--- src/index.ts
+++ src/index.ts
@@ -23,31 +23,38 @@ let unlockThresholds = {
 
 let intervals = {
   autocloner: 1000,
+  autosaving: 3000,
 };
 
 interface SetIntervalIds {
   autocloner: number | null;
+  autosaving: number | null;
 }
 let setIntervalIds: SetIntervalIds = {
   autocloner: null,
+  autosaving: null,
 };
+
+loadSavedGame();
+saveGame();
 
 document.addEventListener("DOMContentLoaded", (_ev) => {
   gameLogic();
 }, { once: true });
 
 function gameLogic() {
-  updateShopPrices();
-
   const cloneButton = assertElementById("cloneButton");
-
   const shopButtons = {
     autocloner: assertElementById("autoclonerBuyButton"),
   };
 
+  // We do a first update on the UI directly at start up
   counterUpdate.cookies();
+  counterUpdate.autocloners();
+  updateShopPrices();
+  checkUnlockables();
 
-  cloneButton?.addEventListener("click", (_ev) => {
+  cloneButton.addEventListener("click", (_ev) => {
     console.log("Button clicked!");
     counters.cookies += 1;
     counterUpdate.cookies();
@@ -55,7 +62,7 @@ function gameLogic() {
     checkUnlockables();
   });
 
-  shopButtons.autocloner?.addEventListener("click", (_ev) => {
+  shopButtons.autocloner.addEventListener("click", (_ev) => {
     // If not enough cookies, return
     if (counters.cookies < currentPrices.autocloner) {
       return;
@@ -87,6 +94,13 @@ function setIntervals() {
     counters.cookies += counters.autocloners;
     counterUpdate.cookies();
   }, intervals.autocloner);
+
+  if (setIntervalIds.autosaving) {
+    clearInterval(setIntervalIds.autosaving);
+  }
+  setIntervalIds.autosaving = setInterval(() => {
+    saveGame();
+  }, intervals.autosaving);
 }
 
 const counterUpdate = {
@@ -136,4 +150,39 @@ function assertElementById(key: string): HTMLElement {
   }
 
   return element;
+}
+
+function loadSavedGame() {
+  const storage = localStorage;
+
+  const cookieCounterItem = storage.getItem("cookieCounter");
+  const autoclonerCounterItem = storage.getItem("autoclonerCounter");
+  const currentPricesJSONItem = storage.getItem("currentPrices");
+
+  if (cookieCounterItem !== null) {
+    counters.cookies = parseInt(cookieCounterItem);
+  }
+  if (autoclonerCounterItem !== null) {
+    counters.autocloners = parseInt(autoclonerCounterItem);
+  }
+  if (currentPricesJSONItem !== null) {
+    let parsedJSON = JSON.parse(currentPricesJSONItem);
+    console.log(parsedJSON);
+    if (typeof parsedJSON !== "object" || !("autocloner" in parsedJSON)) {
+      throw new Error(
+        `Found invalid JSON while trying to load currentPrices from localStorage:\n${currentPricesJSONItem}`,
+      );
+    }
+    currentPrices = parsedJSON;
+  }
+}
+
+function saveGame() {
+  const storage = localStorage;
+
+  storage.setItem("cookieCounter", counters.cookies.toString());
+  storage.setItem("autoclonerCounter", counters.autocloners.toString());
+  storage.setItem("currentPrices", JSON.stringify(currentPrices));
+
+  console.log("Game saved!");
 }
