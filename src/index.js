ff38b163646dd2d7c42986d76884cd4ad5b5c38a
AuraCat <136636937+AuraCat66@users.noreply.github.com>
2025-03-17 01:11:40 +0100

    move logic inside document.addEventListener into a separate function and add variables for autocloners

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 "use strict";
 let cookies = 0;
 
-document.addEventListener("DOMContentLoaded", function () {
+let autocloners = 0;
+let autoclonerCost = 100;
+let autoclonerInterval = 1000;
+let autoclonerUnlockThreshold = 10;
+
+document.addEventListener("DOMContentLoaded", function (_ev) {
+  gameLogic();
+});
+
+function gameLogic() {
   const cookieCounter = document.getElementById("cookieCounter");
   const cloneButton = document.getElementById("cloneButton");
 
-  cloneButton.addEventListener("click", function (ev) {
+  cloneButton.addEventListener("click", function (_ev) {
     console.log("Button clicked!");
     cookies += 1;
     cookieCounter.innerHTML = cookies;
   });
-});
+}
